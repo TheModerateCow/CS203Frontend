9dd40801e20748fe7f8f93132bb75b88a8755e6b
Chia Jin Jun <144515009+jinjunchia@users.noreply.github.com>
2024-11-12 21:19:30 +0800

    small changes to frontend

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -9,12 +9,11 @@ export default function LoginLayout({
 			<div className="hidden bg-[url('/login.png')] bg-cover bg-center sm:block sm:w-[50%] relative">
 				<div className="flex flex-col gap-8 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/4">
 					<div className="h-12 w-12 bg-white rounded-3xl" />
-					<h1 className="text-5xl font-bold">Level the Playing Field</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-						doloribus dolore omnis eos! Voluptates pariatur exercitationem amet
-						soluta ullam eaque! Quia quisquam animi maiores sunt consequuntur,
-						ipsum deserunt placeat praesentium.
+					<h1 className="text-7xl font-bold">Level your Playing Field with TournaX</h1>
+					<p className ="text-2xl">
+					Effortlessly manage your tournaments with the ideal tools. 
+					TournaX offers a powerful set of features, designed for anyone looking to organize professional or 
+					casual tournaments with ease and flexibility.
 					</p>
 				</div>
 			</div>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,21 +4,30 @@ import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
 import ProtectedRoute from "@/components/ProtectedRoutes";
 import Image from "next/image";
+import { useRouter } from "next/navigation"; // Import useRouter
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter(); // Initialize router
+
+  const handleLogoClick = () => {
+    router.push("/dashboard"); // Replace "/dashboard" with your desired path
+  };
+
   return (
     <ProtectedRoute>
       <div className="h-screen flex">
         {/* LEFT */}
         <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
-          <div className="flex items-center justify-center gap-2 lg:justify-start p-2">
-            <Image src="/logo.svg" alt="logo" width={32} height={32} />
-            <span className="hidden lg:block">TournaX</span>
-          </div>
+          <button onClick={handleLogoClick}>
+            <div className="flex items-center justify-center gap-2 lg:justify-start p-2">
+              <Image src="/logo.svg" alt="logo" width={32} height={32} />
+              <span className="hidden lg:block">TournaX</span>
+            </div>
+          </button>
           <Menu />
         </div>
 
